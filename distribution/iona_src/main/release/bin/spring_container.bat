f16bc73ef0a3e0a1b5f5af6769361a684ac1977d
janstey <janstey@12b78b51-92af-460f-be90-f78743893895>
2007-10-29 14:00:17 +0000

    Made fix for unresolving message part in cxf/trunk/distribution/iona_src/main/re lease/containers/spring_container/etc/wsdl/container.wsdl
    Also merging in the following commits from  https://projects.open.iona.com/projects/svn/iona/cxf/trunk
    
    ------------------------------------------------------------------------
    r22352 | janstey | 2007-10-25 16:03:29 -0230 (Thu, 25 Oct 2007) | 1 line
    
    Adding in the manifest generation *before* the archive creation...
    ------------------------------------------------------------------------
    r22348 | janstey | 2007-10-25 14:28:07 -0230 (Thu, 25 Oct 2007) | 1 line
    
    Updated readme.
    ------------------------------------------------------------------------
    r22339 | janstey | 2007-10-25 12:24:11 -0230 (Thu, 25 Oct 2007) | 1 line
    
    Added in manifest file creation for container
    ------------------------------------------------------------------------
    r22338 | janstey | 2007-10-25 11:47:27 -0230 (Thu, 25 Oct 2007) | 1 line
    
    Renaming spring container script (removing .sh extension)
    ------------------------------------------------------------------------
    r22337 | janstey | 2007-10-25 11:46:10 -0230 (Thu, 25 Oct 2007) | 1 line
    
    Adding spring container start scripts
    ------------------------------------------------------------------------  
    
    
    
    git-svn-id: http://fusesource.com/forge/svn/fusesf/branches/cxf-2.0.x-fuse@331 12b78b51-92af-460f-be90-f78743893895

---
+++ distribution/iona_src/main/release/bin/spring_container.bat
@@ -0,0 +1,54 @@
+@echo off
+if "%OS%" == "Windows_NT" setlocal
+rem ----------------------------------------------------------------------------
+rem This script should be used to start the Spring Container from within FUSE Services Framework.
+rem
+rem Environment Variable Prequisites
+rem   CXF_HOME      Must Point to the FUSE Services Framework install directory
+rem   JAVA_HOME        Must Point to your JDk installation directory
+rem ----------------------------------------------------------------------------
+
+rem Parse CXF_HOME to create TMP value that does not contain quotes.
+for /F "delims=" %%i in ("%CXF_HOME%") do set CXF_HOME_TMP=%%~i
+    
+rem Parse JAVA_HOME to create TMP value that does not contain quotes.
+for /F "delims=" %%j in ("%JAVA_HOME%") do set JAVA_HOME_TMP=%%~j
+    
+rem ensure all the needed environment variables are set
+if not "%CXF_HOME_TMP%" == "" goto gotCXFHome
+echo CXF_HOME must point to the FUSE Services Framework install directory.
+echo Please set CXF_HOME and run this script again.
+goto exitWithError
+
+:gotCXFHome
+if not "%JAVA_HOME_TMP%" == "" (
+ rem Strip quotes
+ set JAVA_HOME=%JAVA_HOME:"=%
+) 
+
+if not "%JAVA_HOME%" == "" goto springHomeSet
+echo 
+echo JAVA_HOME must point to your JDK installation
+echo Please set JAVA_HOME and run this script again.
+goto exitWithError
+
+:springHomeSet
+
+echo Setting SPRING_CONTAINER_HOME to %CXF_HOME%\containers\spring_container
+set SPRING_CONTAINER_HOME=%CXF_HOME%\containers\spring_container
+
+set LOGGING_PROPS=%CXF_HOME%\etc\logging.properties
+
+set SPRING_CONTAINER_CLASSPATH=%SPRING_CONTAINER_HOME%/etc;%CXF_HOME%\lib\it-soa-container.jar;%CXF_HOME%\lib\cxf-manifest-incubator.jar;%CLASSPATH%
+
+"%JAVA_HOME%\bin\java" -cp "%SPRING_CONTAINER_CLASSPATH%" -Djava.util.logging.config.file=%LOGGING_PROPS% com.iona.cxf.container.ContainerLauncher  %*
+goto end
+
+:exitWithError
+echo start_container ended with error
+echo Spring Container has not been started successfully
+exit /b 1
+
+:end
+exit /b 0
+
