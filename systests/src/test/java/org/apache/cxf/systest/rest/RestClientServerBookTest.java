90e66a1b9395068b6bbf592ecc7d9d6fc8771675
cruise <cruise@12b78b51-92af-460f-be90-f78743893895>
2008-02-10 23:54:59 +0000

    Merged revisions 34538 via svnmerge from https://projects.open.iona.com/projects/svn/iona/cxf/branches/cxf-2.0.x-fuse
    ................
      r34538 | dkulp | 2008-02-08 14:33:15 -0500 (Fri, 08 Feb 2008) | 20 lines
      
      Merged revisions 34537 via svnmerge from 
      http://projects.open.iona.com/projects/svn/iona/mirrors/apache.org/incubator/cxf/branches/2.0.x-fixes
      
      ........
        r34537 | svk | 2008-02-08 14:09:02 -0500 (Fri, 08 Feb 2008) | 13 lines
        
         r62129@hades (orig r619978):  dkulp | 2008-02-08 13:07:49 -0600
         Merged revisions 619152 via svnmerge from 
         https://svn.apache.org/repos/asf/incubator/cxf/trunk
         
         ........
           r619152 | dkulp | 2008-02-06 16:00:08 -0500 (Wed, 06 Feb 2008) | 4 lines
           
           [CXF-1419, CXF-1418] Patch from Balaji applied (with fixes to pass tests)
           Added flags to AbstractPhaseInterceptor to create unique id
           Delay creation of databinding till needed so we don't create one if it's just going to be discard.  (example: databinding configured in spring)
         ........
         
      ........
    ................
    
    
    git-svn-id: http://fusesource.com/forge/svn/fusesf/branches/cxf-2.0.3-fuse@1144 12b78b51-92af-460f-be90-f78743893895

--- systests/src/test/java/org/apache/cxf/systest/rest/RestClientServerBookTest.java
+++ systests/src/test/java/org/apache/cxf/systest/rest/RestClientServerBookTest.java
@@ -115,7 +115,6 @@ public class RestClientServerBookTest extends AbstractBusClientServerTestBase {
             .getResourceAsStream("resources/expected_get_book123_xmlwrapped.txt");
 
         String expectedString = getStringFromInputStream(expected);
-        //System.out.println("---" + getStringFromInputStream(in));
         assertEquals(expectedString, expectedString, getStringFromInputStream(in)); 
     }
     
