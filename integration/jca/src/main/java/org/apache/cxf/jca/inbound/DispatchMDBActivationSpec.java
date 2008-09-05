0a8ea5f83fbd4866a2d3a7e02e0d0ad302973bc8
wtam <wtam@12b78b51-92af-460f-be90-f78743893895>
2008-09-05 20:43:01 +0000

    [ART-10241] JCA connector does not work in Wepshere 6.1 the inbound demo
    git-svn-id: http://fusesource.com/forge/svn/fusesf/branches/cxf-2.0.x-fuse@2261 12b78b51-92af-460f-be90-f78743893895

---
+++ integration/jca/src/main/java/org/apache/cxf/jca/inbound/DispatchMDBActivationSpec.java
@@ -0,0 +1,44 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.jca.inbound;
+
+/**
+ *
+ * DispatchMDBActivationSpec is for activating a CXF service endpoint facade
+ * that dispatches invocation to a Stateless Session Bean.
+ * 
+ */
+public class DispatchMDBActivationSpec extends MDBActivationSpec {
+
+    private String targetBeanJndiName;
+
+    /**
+     * @param targetBeanJndiName the targetBeanJndiName to set
+     */
+    public void setTargetBeanJndiName(String targetBeanJndiName) {
+        this.targetBeanJndiName = targetBeanJndiName;
+    }
+
+    /**
+     * @return the targetBeanJndiName
+     */
+    public String getTargetBeanJndiName() {
+        return targetBeanJndiName;
+    }
+}
