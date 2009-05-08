36636d195053415e9ba2b578a1901ff3e97a1d51
lpower <lpower@12b78b51-92af-460f-be90-f78743893895>
2009-05-08 20:55:26 +0000

    Merged revisions 12817 via svnmerge from http://fusesource.com/forge/svn/fusesf/mirrors/cxf/branches/2.0.x-fixes
    ........
      r12817 | lpower | 2009-05-08 15:45:59 -0500 (Fri, 08 May 2009) | 18 lines
      
       r17151@hades (orig r773094):  dkulp | 2009-05-08 15:43:45 -0500
       Merged revisions 773080 via svnmerge from 
       https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes
       
       ................
         r773080 | dkulp | 2009-05-08 16:22:47 -0400 (Fri, 08 May 2009) | 9 lines
         
         Merged revisions 773009 via svnmerge from 
         https://svn.apache.org/repos/asf/cxf/trunk
         
         ........
           r773009 | sergeyb | 2009-05-08 10:32:50 -0400 (Fri, 08 May 2009) | 1 line
           
           Creating a timer on demand in JMSContinuation
         ........
       ................
       
    ........
    
    
    git-svn-id: http://fusesource.com/forge/svn/fusesf/branches/cxf-2.0.x-fuse@12837 12b78b51-92af-460f-be90-f78743893895

--- rt/transports/jms/src/main/java/org/apache/cxf/transport/jms/continuations/JMSContinuation.java
+++ rt/transports/jms/src/main/java/org/apache/cxf/transport/jms/continuations/JMSContinuation.java
@@ -51,7 +51,7 @@ public class JMSContinuation implements Continuation {
     private boolean isNew = true;
     private boolean isPending;
     private boolean isResumed;
-    private Timer timer = new Timer();
+    private Timer timer;
     
     public JMSContinuation(Bus b, Message m, MessageObserver observer,
                            Collection<JMSContinuation> cList, 
@@ -134,6 +134,7 @@ public class JMSContinuation implements Continuation {
     }
 
     protected void createTimerTask(long timeout) {
+        timer = new Timer();
         timer.schedule(new TimerTask() {
             public void run() {
                 synchronized (JMSContinuation.this) { 
@@ -146,7 +147,10 @@ public class JMSContinuation implements Continuation {
     }
     
     protected void cancelTimerTask() {
-        timer.cancel();
+        if (timer != null) {
+            timer.cancel();
+            timer.cancel();
+        }
     }
     
     protected void updateContinuations(boolean remove) {
