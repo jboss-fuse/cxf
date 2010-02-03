9c6af76908a25b5075e06d5a9834a5b46614dee2
SergeyB <SergeyB@12b78b51-92af-460f-be90-f78743893895>
2010-02-03 11:39:19 +0000

    Updating ProviderXMLClientServerTest to launch a server in a separate process again
    git-svn-id: http://fusesource.com/forge/svn/fusesf/branches/cxf-2.2.x-fuse@17591 12b78b51-92af-460f-be90-f78743893895

--- systests/jaxws/src/test/java/org/apache/cxf/systest/provider/ProviderXMLClientServerTest.java
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/provider/ProviderXMLClientServerTest.java
@@ -47,7 +47,7 @@ public class ProviderXMLClientServerTest extends AbstractBusClientServerTestBase
     @BeforeClass
     public static void startServers() throws Exception {
         assertTrue("server did not launch correctly",
-                launchServer(XMLServer.class, true));
+                launchServer(XMLServer.class));
     }
 
     @Test
