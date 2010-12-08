ba469766fc4d26caef10ad6c270f1787c0d5b3c2
njiang <njiang@12b78b51-92af-460f-be90-f78743893895>
2010-12-08 10:08:45 +0000

    SF-299 back port the patch of CXF-2939 into 2.2.6-fuse branch
    git-svn-id: http://fusesource.com/forge/svn/fusesf/branches/cxf-2.2.6-fuse@20825 12b78b51-92af-460f-be90-f78743893895

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/AbstractJAXBProvider.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/AbstractJAXBProvider.java
@@ -38,7 +38,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
-import java.util.WeakHashMap;
 import java.util.logging.Logger;
 
 import javax.ws.rs.WebApplicationException;
@@ -93,8 +92,8 @@ public abstract class AbstractJAXBProvider extends AbstractConfigurableProvider
     private static final String JAXB_DEFAULT_NAME = "##default";
     
     private static final String CHARSET_PARAMETER = "charset";
-    private static Map<String, JAXBContext> packageContexts = new WeakHashMap<String, JAXBContext>();
-    private static Map<Class<?>, JAXBContext> classContexts = new WeakHashMap<Class<?>, JAXBContext>();
+    private static Map<String, JAXBContext> packageContexts = new HashMap<String, JAXBContext>();
+    private static Map<Class<?>, JAXBContext> classContexts = new HashMap<Class<?>, JAXBContext>();
    
     private static Set<Class<?>> collectionContextClasses = new HashSet<Class<?>>();
     private static JAXBContext collectionContext; 
