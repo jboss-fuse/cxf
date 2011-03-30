0a13a93050b0f129461e61593614bafd9aadeaaf
Willem Ning Jiang <ningjiang@apache.org>
2011-03-30 14:04:40 +0000

    CXF-3428 Fixed the issue of Context closed twice with Spring setup
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1086958 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/test/java/org/apache/cxf/bus/spring/BusApplicationListenerTest.java
+++ rt/core/src/test/java/org/apache/cxf/bus/spring/BusApplicationListenerTest.java
@@ -48,6 +48,9 @@ public class BusApplicationListenerTest extends Assert {
         listener.postShutdown();
         EasyMock.expectLastCall().times(1);
         EasyMock.replay(listener);
+        AbstractRefreshableApplicationContext context = 
+            (AbstractRefreshableApplicationContext)factory.getApplicationContext();
+        context.close();
         parent.close();
         EasyMock.verify(listener);
     }
