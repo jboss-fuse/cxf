fe869f88769eb5b0581fd156b1b10b1293a0dc9f
Willem Jiang <willem.jiang@gmail.com>
2011-10-18 22:08:48 +0800

    CXF-3858 Fixed the typo with thanks to Glen CXF-3860 CXF sample restful_dispatch Readme.txt need to be updated

--- rt/core/src/main/java/org/apache/cxf/databinding/source/XMLStreamDataReader.java
+++ rt/core/src/main/java/org/apache/cxf/databinding/source/XMLStreamDataReader.java
@@ -140,10 +140,10 @@ public class XMLStreamDataReader implements DataReader<XMLStreamReader> {
         } catch (IOException e) {
             throw new Fault("COULD_NOT_READ_XML_STREAM", LOG, e);
         } catch (XMLStreamException e) {
-            throw new Fault("COULD_NOT_REDA_XML_STREAM_CAUSED_BY", LOG, e,
+            throw new Fault("COULD_NOT_READ_XML_STREAM_CAUSED_BY", LOG, e,
                             e.getClass().getCanonicalName(), e.getMessage());
         } catch (SAXException e) {
-            throw new Fault("COULD_NOT_REDA_XML_STREAM_CAUSED_BY", LOG, e,
+            throw new Fault("COULD_NOT_READ_XML_STREAM_CAUSED_BY", LOG, e,
                             e.getClass().getCanonicalName(), e.getMessage());
         }
     }
@@ -230,7 +230,11 @@ public class XMLStreamDataReader implements DataReader<XMLStreamReader> {
                 return new DOMSource(document);
             }
         } catch (XMLStreamException e) {
+<<<<<<< HEAD
             throw new Fault("COULD_NOT_REDA_XML_STREAM_CAUSED_BY", LOG, e,
+=======
+            throw new Fault("COULD_NOT_READ_XML_STREAM_CAUSED_BY", LOG, e,
+>>>>>>> 2.3.x-fixes
                             e.getClass().getCanonicalName(), e.getMessage());
         }
     }
