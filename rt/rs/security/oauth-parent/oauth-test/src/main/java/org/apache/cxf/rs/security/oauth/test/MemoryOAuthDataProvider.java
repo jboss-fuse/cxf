0f9ee110693b04dca06fe119667a8e17b2e5d42c
Jonathan Anstey <janstey@gmail.com>
2012-02-09 18:18:30 -0330

    Revert "Merged revisions 1236720 via svnmerge from "
    This reverts commit 0b120c2ef011f95cb613630cffb8aca33d915e2b.

--- rt/rs/security/oauth-parent/oauth-test/src/main/java/org/apache/cxf/rs/security/oauth/test/MemoryOAuthDataProvider.java
+++ rt/rs/security/oauth-parent/oauth-test/src/main/java/org/apache/cxf/rs/security/oauth/test/MemoryOAuthDataProvider.java
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-package org.apache.cxf.systest.jaxrs.security.oauth;
+package org.apache.cxf.rs.security.oauth.test;
 
 import java.util.ArrayList;
 import java.util.Collections;
