2acaa7b46856e414bdbfd6394be1700dbb1d86e9
Sergey Beryozkin <sberyozkin@gmail.com>
2017-09-18 16:19:48 +0100

    Support for caching UriTemplates, patch from bcluap applied, This closes #310
    (cherry picked from commit b1de61a126688db3842bd6ea743141ff70753996)
    
    Conflicts:
    	rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/model/URITemplate.java
    
    (cherry picked from commit 45888b76acc7844f154e4e5ee7dc867e610aeaa7)

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/ProviderCache.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/ProviderCache.java
@@ -19,6 +19,8 @@
 
 package org.apache.cxf.jaxrs.provider;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
