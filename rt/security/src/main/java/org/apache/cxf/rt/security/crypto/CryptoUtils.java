1f0c21e48ba0466be30363332dd36272cc11d04c
Freeman Fang <freeman.fang@gmail.com>
2018-04-20 10:38:01 +0800

    [ENTESB-8014]private key default blocksize changed from 128 to 256 since JDK8 161
    (cherry picked from commit 0733b7e08c7779878cff3042bd2cb49177172170)

--- rt/security/src/main/java/org/apache/cxf/rt/security/crypto/CryptoUtils.java
+++ rt/security/src/main/java/org/apache/cxf/rt/security/crypto/CryptoUtils.java
@@ -520,7 +520,13 @@ public final class CryptoUtils {
                 result = c.doFinal(bytes);
             } else {
                 if (blockSize == -1) {
-                    blockSize = secretKey instanceof PublicKey ? 117 : 128;
+                    String javaVersion = System.getProperty("java.version");
+                    if (isJava8Release161OrLater(javaVersion)) {
+                        //the default block size is 256 when use private key under java8 161 later
+                        blockSize = secretKey instanceof PublicKey ? 117 : 256;
+                    } else {
+                        blockSize = secretKey instanceof PublicKey ? 117 : 128;
+                    }
                 }
                 boolean updateRequired = keyProps != null && keyProps.getAdditionalData() != null;
                 int offset = 0;
@@ -542,6 +548,10 @@ public final class CryptoUtils {
         } catch (Exception ex) {
             throw new SecurityException(ex);
         }
+    }
+
+    private static boolean isJava8Release161OrLater(String javaVersion) {
+        return javaVersion.startsWith("1.8.0_") && Integer.parseInt(javaVersion.substring(6)) >= 161;
     }
     
     public static Cipher initCipher(Key secretKey, KeyProperties keyProps, int mode)  throws SecurityException {
