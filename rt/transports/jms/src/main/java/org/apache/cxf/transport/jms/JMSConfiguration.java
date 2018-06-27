925d68d398a5a5140eec3a064d580e061911121b
Freeman Fang <freeman.fang@gmail.com>
2018-06-27 12:31:12 +0800

    [ENTESB-9102]JMS transport does not clean up resources properly in case of an JMSException
    (cherry picked from commit 257a3db4dd5742257c699010d564d8d1d7787fe0)

--- rt/transports/jms/src/main/java/org/apache/cxf/transport/jms/JMSConfiguration.java
+++ rt/transports/jms/src/main/java/org/apache/cxf/transport/jms/JMSConfiguration.java
@@ -243,6 +243,12 @@ public class JMSConfiguration {
         this.connectionFactory = connectionFactory;
     }
 
+    public void resetCachedReplyDestination() {
+        synchronized (this) {
+            this.replyDestinationDest = null;
+        }
+    }
+
     public String getTargetDestination() {
         return targetDestination;
     }
