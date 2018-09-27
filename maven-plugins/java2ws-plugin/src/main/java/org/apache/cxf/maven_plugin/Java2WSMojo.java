ec5549d2e8bd4ff8714e160bc1b065b143778944
Freeman Fang <freeman.fang@gmail.com>
2018-09-27 19:04:38 +0800

    [CXF-7851]NullPointerException in cxf-java2ws-plugin with more than one attachWsdl=true
    (cherry picked from commit fbdb8b7117193b629101966db714cb1fa700cf4c)
    (cherry picked from commit 6ceee085baafa0cc7732e680774d8bddee3013bd)

--- maven-plugins/java2ws-plugin/src/main/java/org/apache/cxf/maven_plugin/Java2WSMojo.java
+++ maven-plugins/java2ws-plugin/src/main/java/org/apache/cxf/maven_plugin/Java2WSMojo.java
@@ -436,7 +436,7 @@ public class Java2WSMojo extends AbstractMojo {
                 
                 boolean hasWsdlAttached = false;
                 for (Artifact a : project.getAttachedArtifacts()) {
-                    if ("wsdl".equals(a.getType()) && classifier.equals(a.getClassifier())) {
+                    if ("wsdl".equals(a.getType()) && classifier != null && classifier.equals(a.getClassifier())) {
                         hasWsdlAttached = true;
                     }
                 }
