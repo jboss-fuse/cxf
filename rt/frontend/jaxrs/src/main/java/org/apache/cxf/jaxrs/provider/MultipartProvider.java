dbf3a52e615c34f0008fa5a0e3405ca0708340b5
Freeman Fang <freeman.fang@gmail.com>
2020-11-23 09:44:22 -0500

    [ENTESB-14976]ensure MultipartProvider works with activation api 1.2

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/MultipartProvider.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/MultipartProvider.java
@@ -445,7 +445,7 @@ public class MultipartProvider extends AbstractConfigurableProvider
                                      Type genericType,
                                      Annotation[] anns,
                                      MediaType contentType) {
-            super(new ByteDataSource("1".getBytes()));
+            super(new ByteDataSource("1".getBytes(), contentType.getType()));
             this.writer = writer;
             this.obj = obj;
             this.cls = cls;
