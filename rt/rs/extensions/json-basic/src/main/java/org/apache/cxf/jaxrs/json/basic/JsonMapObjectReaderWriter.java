9bd971e76252e8ce657697ead9f427c45830f5ee
Freeman Fang <freeman.fang@gmail.com>
2021-07-08 16:24:17 -0400

    fix checkstyle error
    (cherry picked from commit f81159320ebd5517050e8b15307cf0bb482702c1)

--- rt/rs/extensions/json-basic/src/main/java/org/apache/cxf/jaxrs/json/basic/JsonMapObjectReaderWriter.java
+++ rt/rs/extensions/json-basic/src/main/java/org/apache/cxf/jaxrs/json/basic/JsonMapObjectReaderWriter.java
@@ -21,7 +21,6 @@ package org.apache.cxf.jaxrs.json.basic;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UncheckedIOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
