39952608c201331b4b826414253ac352dd261edc
Freeman Fang <freeman.fang@gmail.com>
2024-08-29 14:24:21 -0400

    [CSB-5069]use latest TLSv1.3 as default SSLContext protocol

--- rt/transports/http-undertow/src/main/java/org/apache/cxf/transport/http_undertow/UndertowHTTPServerEngine.java
+++ rt/transports/http-undertow/src/main/java/org/apache/cxf/transport/http_undertow/UndertowHTTPServerEngine.java
@@ -522,7 +522,7 @@ public class UndertowHTTPServerEngine implements ServerEngine, HttpServerEngineS
         }
 
         String proto = tlsServerParameters.getSecureSocketProtocol() == null
-            ? "TLS" : tlsServerParameters.getSecureSocketProtocol();
+            ? "TLSv1.3" : tlsServerParameters.getSecureSocketProtocol();
 
         SSLContext context = tlsServerParameters.getJsseProvider() == null
             ? SSLContext.getInstance(proto)
