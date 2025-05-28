1370ad6450f7ce6317b7f88a7216580a920f09bc
Tom Cunningham <tcunning@redhat.com>
2025-05-28 15:33:13 -0400

    Fix error in testThatNewInnerSpanIsCreatedUsingAsyncInvocation (order of spans) (#681)
    * Fix error in testThatNewInnerSpanIsCreatedUsingAsyncInvocation (order of spans)
    
    * Fix Async test in OpenTelemetry
    
    * Fix order in another test
    
    * Keep the index of the GET url

--- systests/tracing/src/test/java/org/apache/cxf/systest/jaxrs/tracing/opentracing/OpenTracingTracingTest.java
+++ systests/tracing/src/test/java/org/apache/cxf/systest/jaxrs/tracing/opentracing/OpenTracingTracingTest.java
@@ -199,8 +199,22 @@ public class OpenTracingTracingTest extends AbstractClientServerTestBase {
         await().atMost(Duration.ofSeconds(1L)).until(()-> REPORTER.getSpans().size() == 2);
 
         assertThat(REPORTER.getSpans().size(), equalTo(2));
-        assertEquals("Processing books", REPORTER.getSpans().get(0).getOperationName());
-        assertEquals("GET /bookstore/books/async", REPORTER.getSpans().get(1).getOperationName());
+        boolean foundProcessing = false;
+        for (int i = 0; i < 2; i++) {
+            if ("Processing books".equals(REPORTER.getSpans().get(i).getOperationName())) {
+                foundProcessing = true;
+            }
+        }
+        assertTrue("Could not find Processing books in span getOperationName()", foundProcessing);
+
+        boolean foundAsync = false;
+        for (int i = 0; i < 2; i++) {
+            if ("GET /bookstore/books/async".equals(REPORTER.getSpans().get(i).getOperationName())) {
+                foundAsync = true;
+            }
+        }
+        assertTrue("Could not find GET /bookstore/books/async in span getOperationName()", foundAsync);
+
         assertThat(REPORTER.getSpans().get(1).getReferences(), not(empty()));
         assertThat(REPORTER.getSpans().get(1).getReferences().get(0).getSpanContext().getSpanId(),
             equalTo(spanId.getSpanId()));
@@ -225,8 +239,20 @@ public class OpenTracingTracingTest extends AbstractClientServerTestBase {
         await().atMost(Duration.ofSeconds(1L)).until(()-> REPORTER.getSpans().size() == 2);
 
         assertThat(REPORTER.getSpans().size(), equalTo(2));
-        assertThat(REPORTER.getSpans().get(0).getOperationName(), equalTo("Processing books"));
-        assertThat(REPORTER.getSpans().get(1).getOperationName(), equalTo("GET /bookstore/books/async"));
+        boolean foundProcessing = false;
+        for (int i = 0; i < 2; i++) {
+            if ("Processing books".equals(REPORTER.getSpans().get(i).getOperationName())) {
+                foundProcessing = true;
+            }
+        }
+        assertTrue("Could not find Processing books in span getOperationName()", foundProcessing);
+
+        boolean foundAsync = false;
+        for (int i = 0; i < 2; i++) {
+            if ("GET /bookstore/books/async".equals(REPORTER.getSpans().get(i).getOperationName())) {
+                foundAsync = true;
+            }
+        }
     }
 
     @Test
