98ebba983f197b1612cb4b4f187de751a338ecb2
Tom Cunningham <tcunning@redhat.com>
2025-07-18 12:18:42 -0400

    Disable OpenTelemetryTracingTest (span order failing still) (#692)
    * Disable OpenTelemetryTracingTest (span order failing still)
    
    * Disable Brave async tests (#684)
    
    * Disable jaxrs OpenTracingTracingTest async tests
    
    * Add Ignore annotation so junit 4 tests are skipped

--- systests/tracing/src/test/java/org/apache/cxf/systest/jaxrs/tracing/opentelemetry/OpenTelemetryTracingTest.java
+++ systests/tracing/src/test/java/org/apache/cxf/systest/jaxrs/tracing/opentelemetry/OpenTelemetryTracingTest.java
@@ -75,8 +75,10 @@ import io.opentelemetry.semconv.UserAgentAttributes;
 import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
 import org.junit.rules.ExpectedException;
 
 import static org.apache.cxf.systest.jaxrs.tracing.opentelemetry.HasAttribute.hasAttribute;
@@ -91,6 +93,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+@Ignore
+@Disabled("Test is fluky on fuse-jenkins")
 public class OpenTelemetryTracingTest extends AbstractClientServerTestBase {
     public static final String PORT = allocatePort(OpenTelemetryTracingTest.class);
 
@@ -249,6 +253,7 @@ public class OpenTelemetryTracingTest extends AbstractClientServerTestBase {
         assertThat(serverSpan.getInstrumentationScopeInfo().getName(), equalTo("jaxrs-server-test"));
     }
 
+    @Disabled
     @Test
     public void testThatNewInnerSpanIsCreatedUsingAsyncInvocation() throws InterruptedException {
         final Context parentContext = fromRandom();
@@ -269,6 +274,8 @@ public class OpenTelemetryTracingTest extends AbstractClientServerTestBase {
         }
     }
 
+
+    @Disabled
     @Test
     public void testThatOuterSpanIsCreatedUsingAsyncInvocation() {
         final Context parentContext = fromRandom();
@@ -282,6 +289,7 @@ public class OpenTelemetryTracingTest extends AbstractClientServerTestBase {
         }
     }
 
+    @Disabled
     @Test
     public void testThatNewSpanIsCreatedUsingAsyncInvocation() throws InterruptedException {
         final Response r = createWebClient("/bookstore/books/async").get();
@@ -295,6 +303,7 @@ public class OpenTelemetryTracingTest extends AbstractClientServerTestBase {
         assertThat(spans.get(1).getName(), equalTo("GET /bookstore/books/async"));
     }
 
+    @Disabled
     @Test
     public void testThatNewSpanIsCreatedWhenNotProvidedUsingAsyncClient() throws Exception {
         final WebClient client = createWebClient("/bookstore/books",
@@ -313,6 +322,7 @@ public class OpenTelemetryTracingTest extends AbstractClientServerTestBase {
         assertThat(otelRule.getSpans().get(2).getKind(), equalTo(SpanKind.CLIENT));
     }
 
+    @Disabled
     @Test
     public void testThatNewSpansAreCreatedWhenNotProvidedUsingMultipleAsyncClients() throws Exception {
         final WebClient client = createWebClient("/bookstore/books",
@@ -380,6 +390,7 @@ public class OpenTelemetryTracingTest extends AbstractClientServerTestBase {
         assertThat(otelRule.getSpans().get(3).getParentSpanContext().isValid(), equalTo(false));
     }
 
+    @Disabled
     @Test
     public void testThatProvidedSpanIsNotDetachedWhenActiveUsingAsyncClient() throws Exception {
         final WebClient client = createWebClient("/bookstore/books",
