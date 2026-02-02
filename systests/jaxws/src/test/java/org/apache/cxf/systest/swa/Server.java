aedf88b3992541efea2e7c8197f4566d10d9231a
Freeman Fang <freeman.fang@gmail.com>
2026-02-02 12:35:25 -0500

    accommodate undertow 2.3.22 changes in CXF

--- systests/jaxws/src/test/java/org/apache/cxf/systest/swa/Server.java
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/swa/Server.java
@@ -33,6 +33,7 @@ public class Server extends AbstractBusTestServerBase {
         String address = "http://localhost:" + port + "/swa";
         EndpointImpl ep;
         try {
+            System.setProperty("org.apache.cxf.transports.http_undertow.MultiPartMaxEntitySize", "-1");
             ep = (EndpointImpl) Endpoint.create(new SwANoMimeServiceImpl());
             ep.setWsdlLocation("classpath:wsdl/swa-mime-nomime.wsdl");
             ep.publish(address + "-nomime");
