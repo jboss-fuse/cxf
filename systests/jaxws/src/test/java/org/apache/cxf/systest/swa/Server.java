58b584c954ac301956a3fbb6b3baad02a908518f
Freeman Fang <freeman.fang@gmail.com>
2026-02-18 12:42:43 -0500

    fix up failed test in cxf-4.1.5 production branch
    (cherry picked from commit 81381c30caa4693c9a00b5be8eb37d82fffa4e65)

--- systests/jaxws/src/test/java/org/apache/cxf/systest/swa/Server.java
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/swa/Server.java
@@ -20,6 +20,7 @@
 package org.apache.cxf.systest.swa;
 
 import jakarta.xml.ws.Endpoint;
+import org.apache.cxf.attachment.AttachmentDeserializer;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.testutil.common.AbstractBusTestServerBase;
 import org.apache.cxf.testutil.common.TestUtil;
@@ -33,6 +34,7 @@ public class Server extends AbstractBusTestServerBase {
         String address = "http://localhost:" + port + "/swa";
         EndpointImpl ep;
         try {
+            System.setProperty(AttachmentDeserializer.ATTACHMENT_MAX_SIZE, "-1");
             ep = (EndpointImpl) Endpoint.create(new SwANoMimeServiceImpl());
             ep.setWsdlLocation("classpath:wsdl/swa-mime-nomime.wsdl");
             ep.publish(address + "-nomime");
